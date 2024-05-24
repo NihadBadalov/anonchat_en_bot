2f03c17b9dc89080e50a09f0bef0963b70365f7f
NihadBadalov <32594553+NihadBadalov@users.noreply.github.com>
2024-05-24 22:12:28 +0200

    chore: comment out the examples

--- commands/inputexample.go
+++ commands/inputexample.go
@@ -2,26 +2,24 @@ package commands
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
-
-	"anonchat_en_bot/utils"
 )
 
 func (ce *CommandExecutor) ExecuteInputexample(ctx context.Context, b *bot.Bot, update *models.Update, additionalContext *context.Context) {
-	user_input := utils.UserInput(
-		"Whatcha name, bruv?! Got 10 seconds to ansa",
-		10,
-		ctx,
-		b,
-		update,
-		additionalContext,
-	)
-
-	b.SendMessage(ctx, &bot.SendMessageParams{
-		ChatID: update.Message.Chat.ID,
-		Text:   fmt.Sprintf("%s? Noice name, bruv, innit?", user_input),
-	})
+  return
+	// user_input := utils.UserInput(
+	// 	"Whatcha name, bruv?! Got 10 seconds to ansa",
+	// 	10,
+	// 	ctx,
+	// 	b,
+	// 	update,
+	// 	additionalContext,
+	// )
+	//
+	// b.SendMessage(ctx, &bot.SendMessageParams{
+	// 	ChatID: update.Message.Chat.ID,
+	// 	Text:   fmt.Sprintf("%s? Noice name, bruv, innit?", user_input),
+	// })
 }
--- commands/kbexample.go
+++ commands/kbexample.go
@@ -2,15 +2,13 @@ package commands
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
-
-	"anonchat_en_bot/utils"
 )
 
 func (ce *CommandExecutor) ExecuteDayword(ctx context.Context, b *bot.Bot, update *models.Update, additionalContext *context.Context) {
+  return
 	// If not subscribed:
 	//   1. Do you want to subscribe to the word of the day?
 	//   2. If yes, confirm the subscription and show today's word
@@ -18,13 +16,13 @@ func (ce *CommandExecutor) ExecuteDayword(ctx context.Context, b *bot.Bot, updat
 	// Add a note below that if they wish to unsubscribe, they can do so in settings,
 	// typing /settings
 
-	btns := []utils.KeyboardButton{
-		{Name: "Grapes", Inline: true},
-		{Name: "Apples", Inline: true},
-	}
-	choice := utils.UserKeyboard("Do you like:", btns, 10, ctx, b, update, additionalContext)
-	b.SendMessage(ctx, &bot.SendMessageParams{
-		ChatID: update.Message.Chat.ID,
-		Text:   fmt.Sprintf("%s? Meh, sure...", choice),
-	})
+	// btns := []utils.KeyboardButton{
+	// 	{Name: "Grapes", Inline: true},
+	// 	{Name: "Apples", Inline: true},
+	// }
+	// choice, _ := utils.UserKeyboard("Do you like:", btns, 10, ctx, b, update, additionalContext)
+	// b.SendMessage(ctx, &bot.SendMessageParams{
+	// 	ChatID: update.Message.Chat.ID,
+	// 	Text:   fmt.Sprintf("%s? Meh, sure...", choice),
+	// })
 }
